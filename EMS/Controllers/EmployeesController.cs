d63e625d9931b129a8862241bd17cbdf0e74bd5e
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-12-05 22:11:06 +0530

    Added web project and webjob

---
+++ EMS/Controllers/EmployeesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EMS.Models;
+using EMS.SERVICES;
+
+namespace EMS.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+        private EmployeeService employeeService = new EmployeeService();
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(employeeService.GetEmployees());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = employeeService.GetEmployeeDetails(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeId,Name,Alias,DateOfBirth,DateOfJoining,IsWorking,Gender")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                var result=employeeService.AddEmployee(employee);
+                if(result)
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = employeeService.GetEmployeeDetails(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeId,Name,Alias,DateOfBirth,DateOfJoining,IsWorking,Gender")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = employeeService.UpdateEmployee(employee);
+                if(result)
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = employeeService.GetEmployeeDetails(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+      
+    }
+}
